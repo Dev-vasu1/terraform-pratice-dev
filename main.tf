ac414e23e0e067fb8e7d4b056c284e52bf416e28
Vasudev ummaneni <vasunaidu925@gmail.com>
2026-09-16 12:59:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "name" {
-    ami = "ami-081b0a6eac00b4f53"
+    ami = "ami-0e34b50e714a297f1"
     instance_type = "7i-flex.large"
     subnet_id = "subnet-0ea97b59003ca590f"
 }
